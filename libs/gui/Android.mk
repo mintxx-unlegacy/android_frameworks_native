02d1cf57d0e5483963d5cb75557a29ae511efa4b
corphish <d97.avinaba@gmail.com>
2016-06-27 17:31:26 +0530

    gui/surfaceflinger: Bring back mHeap based screenshots Credits - @pawitp
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- libs/gui/Android.mk
+++ libs/gui/Android.mk
@@ -80,6 +80,11 @@ LOCAL_SHARED_LIBRARIES := \
 	libui \
 	libutils \
 	liblog
+	
+ifeq ($(BOARD_USE_MHEAP_SCREENSHOT),true)
+    LOCAL_CFLAGS += -DUSE_MHEAP_SCREENSHOT
+endif
+
 
 
 LOCAL_MODULE := libgui

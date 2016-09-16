f4b3f86fbf89ae34e7b90f81b70fd63a80342035
corphish <d97.avinaba@gmail.com>
2016-09-16 22:46:53 +0530

    surfaceflinger: Include gl.h in surfaceflinger_hwc1
    Change-Id: I67f3bbcfd41e0cee9eb7ed1f090a75813fb653e0
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- services/surfaceflinger/SurfaceFlinger_hwc1.cpp
+++ services/surfaceflinger/SurfaceFlinger_hwc1.cpp
@@ -30,7 +30,13 @@
 
 #include <EGL/egl.h>
 
+
 #include <cutils/iosched_policy.h>
+
+#ifdef USE_MHEAP_SCREENSHOT
+#include <GLES/gl.h>
+#endif
+
 #include <cutils/log.h>
 #include <cutils/properties.h>
 

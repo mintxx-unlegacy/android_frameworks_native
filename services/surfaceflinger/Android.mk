31b2d2d8d26c676797d5e59486277257d3230b55
corphish <d97.avinaba@gmail.com>
2016-10-08 11:42:25 +0530

    surfaceflinger: Bring back EGL_NEEDS_FNW
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -142,6 +142,10 @@ ifeq ($(BOARD_USE_BGRA_8888),true)
     LOCAL_CFLAGS += -DUSE_BGRA_8888
 endif
 
+ifeq ($(BOARD_EGL_NEEDS_FNW),true)
+    LOCAL_CFLAGS += -DEGL_NEEDS_FNW
+endif
+
 LOCAL_CFLAGS += -fvisibility=hidden -Werror=format
 LOCAL_CFLAGS += -std=c++14
 
